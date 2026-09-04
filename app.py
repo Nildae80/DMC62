0060b044097c8736b6ddf16d9231ea57551ba6ee
Nildae80 <nildae80@gmail.com>
2026-09-03 22:17:37 -0500

    Add module selection and greetings in app.py

--- app.py
+++ app.py
@@ -6,3 +6,14 @@ st.write("Elaborado por: Nilda Echevarria")
 
 modulos = st.sidebar.selectbox("Seleccione el modulo",["Listas", "Arreglos", "Funciones", "POO"])
 
+if modulos == "Listas":
+  st.write("Te encuentras en el modulo de listas")
+  
+elif modulos == "Arreglos":
+  st.write("Bienvenidos al modulo de arreglos")
+
+elif modulos == "Funciones":
+  st.write("Bienvenidos al modulo de funciones")
+
+else
+  st.write("Bienvenidos al modulo de POO")
