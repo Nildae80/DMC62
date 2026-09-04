10b98579934faefb8634519604dc77855fbe721b
Nildae80 <nildae80@gmail.com>
2026-09-03 22:23:26 -0500

    Refactor number input and list generation for ranges
    Updated number input for initial and final range values to include default values and generate a list based on the input.

--- app.py
+++ app.py
@@ -8,10 +8,12 @@ modulos = st.sidebar.selectbox("Seleccione el modulo",["Listas", "Arreglos", "Fu
 
 if modulos == "Listas":
   st.write("Te encuentras en el modulo de listas")
-  valor_inicial = st.number_input("Ingresa tu valor inicial del rango")
-  valor_final = st.number_input("Ingresa tu valor final del rango")
-
-
+  
+  valor_inicial = int(st.number_input("Ingresa tu valor inicial del rango", value=0))
+  valor_final = int(st.number_input("Ingresa tu valor final del rango", value=10))
+  
+  lista = list(range(valor_inicial, valor_final))
+  st.write(lista)
   
 elif modulos == "Arreglos":
   st.write("Bienvenidos al modulo de arreglos")
