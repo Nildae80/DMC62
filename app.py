f4af02988fe84f1332b2aa48bda1aa99a49cf678
Nildae80 <nildae80@gmail.com>
2026-09-03 22:19:51 -0500

    Add input fields for range values in 'Listas' module
    Added number input fields for initial and final range values in the 'Listas' module.

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ modulos = st.sidebar.selectbox("Seleccione el modulo",["Listas", "Arreglos", "Fu
 
 if modulos == "Listas":
   st.write("Te encuentras en el modulo de listas")
+  valor_inicial = st.number_input("Ingresa tu valor inicial del rango")
+  valor_final = st.number_input("Ingresa tu valor final del rango")
+
+
   
 elif modulos == "Arreglos":
   st.write("Bienvenidos al modulo de arreglos")
