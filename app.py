dcbabc1d71cf6ff47f81b69dc26a8f4988de8dd5
Nildae80 <nildae80@gmail.com>
2026-09-10 22:20:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import numpy as np
+import libreria_funciones as lf
 
 st.title("Especialización Python for Analytics")
 st.sidebar.title("Parámetros")
@@ -30,6 +31,13 @@ elif modulos == "Arreglos":
 
 elif modulos == "Funciones":
   st.write("Bienvenidos al modulo de funciones")
-
+  
+  capital_i = st.number_input("Ingrese capital inicial", min_value = 0, max_value = 100000, value = 1000)
+  aporte_m = st.number_input("Ingrese el aporte mensual", min_value = 0, max_value = 10000, value = 100)
+  tasa_a = st.slider("Ingrese tasa anual", min_value = 0.01, max_value = 1.0, value = 0.05)
+  anios = st.slider("Ingrese tiempo", min_value = 1, max_value = 20, value = 2)
+  resultado_valor_futuro = lf.valor_futuro_inversion(capital_i,aporte_m,tasa_a,anios)
+  st.write("El resultado de tu valor futuro de inversion es: ",resultado_valor_futuro)
+  
 else:
   st.write("Bienvenidos al modulo de POO")
